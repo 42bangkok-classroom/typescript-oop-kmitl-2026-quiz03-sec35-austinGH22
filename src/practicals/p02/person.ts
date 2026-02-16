f4d8e5db35df8a18fd23ed3825c214ce78839c89
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-02-16 09:23:31 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,21 @@
-export class Person {}
+export class Person {
+    private age:number= 0
+    public firstname:string = ""
+    public lastname:String = ""
+    public static COUNTRY:string = "Thailand"
+
+    constructor(){
+    }
+
+    setAge(age:number){
+        this.age = age
+    }
+
+    getAge(){
+        return this.age 
+    }
+
+    getFullName(){
+        return this.firstname + " " + this.lastname
+    }
+}
