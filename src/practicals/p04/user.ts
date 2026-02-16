664b8bf1ba02994604274b83666dec4bf5af66f5
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-02-16 10:05:44 +0700

    p04

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,4 +1,37 @@
 export class User {
     
-    constructor(){}
+    public username: string;     
+    private password: string;    
+    private static LOGIN_ATTEMPTS: number = 0;
+
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+
+
+    private validatePassword(password: string): boolean {
+        if(this.password === password){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    public login(password: string): boolean {
+        if (this.validatePassword(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public getLoginAttempts(): number {
+        User.LOGIN_ATTEMPTS = User.LOGIN_ATTEMPTS + 1;
+        return User.LOGIN_ATTEMPTS;
+    }
 }
+
